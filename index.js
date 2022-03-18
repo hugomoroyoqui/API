74157434221e1298f05cc53327d69de4fec53429
Hugo Moroyoqui <hugo_moroyoqui@hotmail.com>
2022-03-17 17:17:09 -0700

    init

---
+++ index.js
@@ -0,0 +1,31 @@
+try {
+    const express = require('express');
+    const app = express();
+    const morgan = require('morgan');
+    const bodyParser = require('body-parser');
+    const cors = require('cors');
+
+    const connection = require('./database/databaseprueba');
+
+    const port = process.env.PORT || 3000;
+    
+    //Settings
+    app.set('port', port);
+    
+    //Middlewares
+    app.use(morgan('dev')); 
+    app.use(bodyParser.json());
+
+    app.use(cors({ origin: '*'}));    
+    
+    //Routes
+    
+    
+    //Server
+    app.listen(port, () => {
+        console.log('server on port: ' + port); 
+    }); 
+
+} catch (error) {
+    
+}
